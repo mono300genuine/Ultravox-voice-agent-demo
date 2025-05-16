07a0fc13d0795b97299dc9a72a6474c87540eb25
mono300genuine <monolinker31@gmail.com>
2025-05-16 16:16:28 -0700

    fix: agent id

--- app/demo-config.ts
+++ app/demo-config.ts
@@ -137,7 +137,7 @@ export const demoConfig: DemoConfig = {
     model: "fixie-ai/ultravox-70B",
     languageHint: "en",
     selectedTools: selectedTools,
-    voice: "Mark",
+    voice: "terrence",
     temperature: 0.4,
     agentId: "8ed4a142-f40c-4f69-8e16-2dff09633a41"
   }
